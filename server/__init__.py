4e29a363460d5ac0ed12e13e6e6bbd9d124363fb
Ahmed Ibrahim <ib153507@gmail.com>
2022-06-07 19:58:32 +0200

    Return projects in a json format

--- server/__init__.py
+++ server/__init__.py
@@ -6,7 +6,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
          SECRET_KEY='dev',
-         DATABASE=os.path.join(app.instance_path, 'server.sqlite')
+         DATABASE=os.path.join(app.instance_path, 'server.sqlite'),
+         TEST_DATA=os.path.join(app.root_path, 'data/test_data.json')
     )
 
     if test_config is None:
--- server/project.py
+++ server/project.py
@@ -1,5 +1,6 @@
 import functools
-from flask import Blueprint, request, jsonify, url_for
+from flask import Blueprint, request, current_app
+import json
 from server.db import get_db
 
 bp = Blueprint('projects', __name__, url_prefix='/projects')
@@ -7,4 +8,10 @@ bp = Blueprint('projects', __name__, url_prefix='/projects')
 @bp.route('/', methods=['GET'])
 def get_projects():
     if request.method == 'GET':
-        return jsonify({})
+        with current_app.open_resource(current_app.config['TEST_DATA']) as f:
+            response = current_app.response_class(
+                response=f.read().decode('utf-8'),
+                status=200,
+                mimetype='application/json'
+            )
+            return response
